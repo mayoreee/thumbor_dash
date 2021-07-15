5b6b9172e1d0fc9b9475da6842eeb6a14ec8a198
Mayowa Egbinola <mayowaegbinola@gmail.com>
2021-07-15 19:57:47 +0100

    update document request data

--- thumbor_dash/dapi/dapiclient.py
+++ thumbor_dash/dapi/dapiclient.py
@@ -14,21 +14,22 @@ stub = platform_pb2_grpc.PlatformStub(channel)
 
 
 
-def get_documents(contract_id, type, options):
+def get_documents(data) :
     # Get Document
-    contract_id_bytes = contract_id.encode('UTF-8')
+    contract_id_bytes = data.contract_id.encode('UTF-8')
     contract_id_base64 = base64.b64encode(contract_id_bytes)
 
     document_request = platform_pb2.GetDocumentsRequest()
     document_request.data_contract_id = contract_id_base64
-    document_request.document_type = type
-    document_request.limit = 2
-    # document_request.where = # Requires cbor (found in dapi-client)
+    document_request.document_type = data.document_type
+    document_request.where = cbor2.dumps(data.where)
+    document_request.limit = 2 # Only retrieve 1 document
   
     docs = stub.getDocuments(document_request, GRPC_REQUEST_TIMEOUT)
     # print(docs)
     for d in docs.documents:
         print('Document cbor: {}\n'.format(cbor2.loads(d)))
+    return docs
 
 
 
