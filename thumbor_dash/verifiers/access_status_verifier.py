4c9ca508f8460c22651bf1fd64eb0a1754eb9c12
Mayowa Egbinola <mayowaegbinola@gmail.com>
2021-07-19 01:46:33 +0100

    update verifiers

---
+++ thumbor_dash/verifiers/access_status_verifier.py
@@ -0,0 +1,4 @@
+''' Check user ban status '''
+def verifyImageSize(doc, field):
+    #TODO: add logic when user moderation feature is available
+    return True
--- thumbor_dash/verifiers/thumbnail_size_verifier.py
+++ thumbor_dash/verifiers/thumbnail_size_verifier.py
@@ -1,10 +1,4 @@
-
-def verifyURLField(doc, url):
-    if doc.field == url :
-        return True
-    else:
-        return False
-
+''' Check whether the requested thumbnail size is within allowed bounds'''
 def verifyThumbnailSize(thumbnail_width, thumbnail_height, MIN_WIDTH, MIN_HEIGHT, MAX_WIDTH, MAX_HEIGHT):
     if thumbnail_width >= MIN_WIDTH & thumbnail_width <= MAX_WIDTH & thumbnail_height >= MIN_HEIGHT & thumbnail_height <= MAX_HEIGHT:
         return True
