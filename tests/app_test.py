0d4a836709c2a79cea3e812aa6a866d4a20163eb
mayoreee <mayowaegbinola@gmail.com>
2021-08-21 20:07:56 +0100

    update thumbor_dash test

--- tests/app_test.py
+++ tests/app_test.py
@@ -11,32 +11,32 @@ class AppTestCase(TestCase):
         expect(app).not_to_be_null()
         expect(app.context).to_equal(self.context)
 
-    # def test_can_get_handlers(self):
-    #     ctx = self.get_context()
-    #     ctx.config.UPLOAD_ENABLED = False
-    #     ctx.config.USE_BLACKLIST = False
-    #     ctx.config.HEALTHCHECK_ROUTE = "/health"
-    #     app = ThumborDashServiceApp(ctx)
+    def test_can_get_handlers(self):
+        ctx = self.get_context()
+        ctx.config.UPLOAD_ENABLED = False
+        ctx.config.USE_BLACKLIST = False
+        ctx.config.HEALTHCHECK_ROUTE = "/health"
+        app = ThumborDashServiceApp(ctx)
 
-    #     handlers = app.get_handlers()
-    #     expect(handlers).to_length(2)
-    #     expect(handlers[0][0]).to_equal(r"/health")
-    #     expect(handlers[1][0]).to_equal(Url.regex())
+        handlers = app.get_handlers()
+        expect(handlers).to_length(2)
+        expect(handlers[0][0]).to_equal(r"/health")
+        expect(handlers[1][0]).to_equal(ThumborDashUrl.regex())
 
-    # def test_can_get_handlers_with_upload(self):
-    #     ctx = self.get_context()
-    #     ctx.config.UPLOAD_ENABLED = True
-    #     ctx.config.USE_BLACKLIST = False
-    #     app = ThumborDashServiceApp(ctx)
+    def test_can_get_handlers_with_upload(self):
+        ctx = self.get_context()
+        ctx.config.UPLOAD_ENABLED = True
+        ctx.config.USE_BLACKLIST = False
+        app = ThumborDashServiceApp(ctx)
 
-    #     handlers = app.get_handlers()
-    #     expect(handlers).to_length(4)
+        handlers = app.get_handlers()
+        expect(handlers).to_length(4)
 
-    # def test_can_get_handlers_with_blacklist(self):
-    #     ctx = self.get_context()
-    #     ctx.config.UPLOAD_ENABLED = False
-    #     ctx.config.USE_BLACKLIST = True
-    #     app = ThumborDashServiceApp(ctx)
+    def test_can_get_handlers_with_blacklist(self):
+        ctx = self.get_context()
+        ctx.config.UPLOAD_ENABLED = False
+        ctx.config.USE_BLACKLIST = True
+        app = ThumborDashServiceApp(ctx)
 
-    #     handlers = app.get_handlers()
-    #     expect(handlers).to_length(3)
+        handlers = app.get_handlers()
+        expect(handlers).to_length(3)
