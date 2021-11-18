af9c937c97b44d3c21ea9a093255b8e289b5a8e7
mayoreee <mayowaegbinola@gmail.com>
2021-11-18 18:02:41 +0100

    Fix seed_ip and mn_ip

--- thumbor_dash/handlers/imaging.py
+++ thumbor_dash/handlers/imaging.py
@@ -21,11 +21,13 @@ class ThumborDashImagingHandler(ImagingHandler):
         error_handler = ErrorHandler(config)
        
         MN_IP = None
-        SEED_IP=config.SEED_IP   
-        MN_LIST = str(config.get("MN_LIST")).split(",")
+        SEED_IP=config.get("SEED_IP")   
+        MN_LIST=config.get("MN_LIST")
+        
 
-        if ((config.get("SEED_IP") is None)  or (os.getenv("SEED_IP") is None)) or (MN_LIST[0] is not "None"):                 
-             MN_IP = random.choice(MN_LIST) 
+        if ((config.get("SEED_IP") is None)  or (os.getenv("SEED_IP") is None)) or (MN_LIST is not None):   
+             CONVERTED_MN_LIST = str(config.get("MN_LIST")).split(",")              
+             MN_IP = random.choice(CONVERTED_MN_LIST) 
         
 
         if self.context.config.MAX_ID_LENGTH > 0:
